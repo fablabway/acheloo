15c0bda62ff578c267a4ece21d7871272be9d67e
fablabway <68069401+fablabway@users.noreply.github.com>
2020-07-10 14:07:08 +0200

    Add files via upload
    comments added

--- oradrill.sql
+++ oradrill.sql
@@ -14,110 +14,174 @@
 SET SERVEROUTPUT ON SIZE 1000000
 
 DECLARE
-  WRELCODE  VARCHAR2(30) := 'WE';
-  WOPERATOR VARCHAR2(30) := 'mauro';
-  WOBJOWNE  VARCHAR2(30) := 'MYOWNER';     -- OWNER
-	WSEARCH   VARCHAR2(30) := '\''[1UPDJ297]'; -- LOOKING FOR...
+    wrelcode    VARCHAR2(30) := 'WE';
+    woperator   VARCHAR2(30) := 'mauro';
+    wobjowne    VARCHAR2(30) := 'MYOWNER';     					-- OWNER
+    wcolsep     VARCHAR2(1) := ';';            					-- COLUMN SEPARATOR
+    wclobtmp    CLOB;
+    wlenlob     NUMBER;                          					-- CLOB SIZE
+    wnumline    NUMBER;                          					-- LINE COUNTER
+    woffset     PLS_INTEGER := 1;                   					-- SINCE LAST CR
+    wonerow     VARCHAR2(32767);           					-- EXTRACTED FROM CLOB
+    wrowlen     NUMBER;
+    wsearch     VARCHAR2(30) := '\''[1UPDJ297]'; 					-- LOOKING FOR...
 --  WSEARCH   VARCHAR2(100) := 'TABLE1|VIEW1|OTHEROBJ|ANDSOON'; -- LOOKING FOR...
--- WSEARCH   VARCHAR2(100) := ' TABLE1|VIEW1|^NEGOZI'; -- LOOKING FOR...
--- WSEARCH   VARCHAR2(100) := 'TIPO_NEGOZI'; -- LOOKING FOR...
-
-
-  WCOLSEP   VARCHAR2(1)  := ';';            -- COLUMN SEPARATOR
-  WCLOBTMP CLOB;
-  WLENLOB  NUMBER;                          -- CLOB SIZE
-  WNUMLINE NUMBER;                          -- LINE COUNTER
-  WOFFSET PLS_INTEGER:=1;                   -- SINCE LAST CR
-  WONEROW        VARCHAR2(32767);           -- EXTRACTED FROM CLOB
-  WROWLEN        NUMBER;
+-- WSEARCH   VARCHAR2(100) := ' TABLE1|VIEW1|^NEGOZI'; 			-- LOOKING FOR...
+-- WSEARCH   VARCHAR2(100) := 'mystring'; 					-- LOOKING FOR...
 BEGIN
-  FOR WREC IN
-  (SELECT OWNER,
-    VIEW_NAME ,
-    TEXT
-  FROM DBA_VIEWS
-  WHERE 1=1
-  AND REGEXP_LIKE(OWNER,WOBJOWNE,'i')
-  )
-  LOOP
-    IF REGEXP_LIKE (WREC.TEXT,WSEARCH,'i') THEN
+    FOR wrec IN (
+        SELECT
+            owner,
+            view_name,
+            text
+        FROM
+            dba_views
+        WHERE
+            1 = 1
+            AND   REGEXP_LIKE ( owner,
+            wobjowne,
+            'i' )
+    ) LOOP
+        IF
+            regexp_like(wrec.text,wsearch,'i')
+        THEN
       -- START DDL-METADATA AND EXTRACTS THE INVOLVED ROWS
-      DBMS_METADATA.SET_TRANSFORM_PARAM(DBMS_METADATA.SESSION_TRANSFORM,'EMIT_SCHEMA',false);
-      DBMS_METADATA.SET_TRANSFORM_PARAM(DBMS_METADATA.SESSION_TRANSFORM,'SEGMENT_CREATION',false);
-      DBMS_METADATA.SET_TRANSFORM_PARAM(DBMS_METADATA.SESSION_TRANSFORM,'PRETTY',true);
-      DBMS_METADATA.SET_TRANSFORM_PARAM(DBMS_METADATA.SESSION_TRANSFORM,'STORAGE',false);
-      SELECT DBMS_METADATA.GET_DDL('VIEW',WREC.VIEW_NAME,WREC.OWNER) MYROW
-      INTO WCLOBTMP
-      FROM DUAL;
-      WLENLOB      := DBMS_LOB.getLength(WCLOBTMP);
-      WNUMLINE     :=1;
-      WHILE WOFFSET<=WLENLOB
-      LOOP
-        WROWLEN  :=instr(WCLOBTMP,chr(10),WOFFSET)-WOFFSET;
-        IF WROWLEN<0 THEN
-          WROWLEN:=WLENLOB+1-WOFFSET;
+            dbms_metadata.set_transform_param(dbms_metadata.session_transform,'EMIT_SCHEMA',false);
+            dbms_metadata.set_transform_param(dbms_metadata.session_transform,'SEGMENT_CREATION',false);
+            dbms_metadata.set_transform_param(dbms_metadata.session_transform,'PRETTY',true);
+            dbms_metadata.set_transform_param(dbms_metadata.session_transform,'STORAGE',false);
+            SELECT
+                dbms_metadata.get_ddl('VIEW',wrec.view_name,wrec.owner) myrow
+            INTO
+                wclobtmp
+            FROM
+                dual;
+
+            wlenlob := dbms_lob.getlength(wclobtmp);
+            wnumline := 1;
+            WHILE woffset <= wlenlob LOOP
+                wrowlen := instr(wclobtmp,chr(10),woffset) - woffset;
+                IF
+                    wrowlen < 0
+                THEN
+                    wrowlen := wlenlob + 1 - woffset;
+                END IF;
+                wonerow := substr(wclobtmp,woffset,wrowlen);
+                IF
+                    regexp_like(wonerow,wsearch,'i')
+                THEN
+                    dbms_output.put_line(wrec.view_name
+                    || wcolsep
+                    || wrec.owner
+                    || wcolsep
+                    || 'VIEW'
+                    || wcolsep
+                    || TO_CHAR(wnumline)
+                    || wcolsep
+                    || ltrim(rtrim(wonerow) ) );
+                END IF;
+
+                woffset := woffset + wrowlen + 1;
+                wnumline := wnumline + 1;
+            END LOOP;
+
         END IF;
-        WONEROW:=SUBSTR(WCLOBTMP,WOFFSET,WROWLEN);
-        IF REGEXP_LIKE(WONEROW,WSEARCH,'i') THEN
-          dbms_output.put_line( WREC.VIEW_NAME||WCOLSEP||WREC.OWNER||WCOLSEP||'VIEW'||WCOLSEP|| TO_CHAR(WNUMLINE)||WCOLSEP||LTRIM(RTRIM(WONEROW)));
-        END IF;
-        WOFFSET  :=WOFFSET   +WROWLEN+1;
-        WNUMLINE := WNUMLINE + 1;
-      END LOOP;
-    END IF;
-  END LOOP;
+    END LOOP;
   --**********************
-  FOR WREC IN
-  (SELECT OWNER,
-    MVIEW_NAME ,
-    QUERY
-  FROM DBA_MVIEWS
-  WHERE 1=1
-  AND REGEXP_LIKE(OWNER,WOBJOWNE,'i')
-  )
-  LOOP
-    IF REGEXP_LIKE (WREC.QUERY,WSEARCH,'i') THEN
+
+    FOR wrec IN (
+        SELECT
+            owner,
+            mview_name,
+            query
+        FROM
+            dba_mviews
+        WHERE
+            1 = 1
+            AND   REGEXP_LIKE ( owner,
+            wobjowne,
+            'i' )
+    ) LOOP
+        IF
+            regexp_like(wrec.query,wsearch,'i')
+        THEN
       -- START DDL-METADATA AND EXTRACTS THE INVOLVED ROWS
-      DBMS_METADATA.SET_TRANSFORM_PARAM(DBMS_METADATA.SESSION_TRANSFORM,'EMIT_SCHEMA',false);
-      DBMS_METADATA.SET_TRANSFORM_PARAM(DBMS_METADATA.SESSION_TRANSFORM,'SEGMENT_CREATION',false);
-      DBMS_METADATA.SET_TRANSFORM_PARAM(DBMS_METADATA.SESSION_TRANSFORM,'PRETTY',true);
-      DBMS_METADATA.SET_TRANSFORM_PARAM(DBMS_METADATA.SESSION_TRANSFORM,'STORAGE',false);
-      SELECT DBMS_METADATA.GET_DDL('MATERIALIZED_VIEW',WREC.MVIEW_NAME,WREC.OWNER) MYROW
-      INTO WCLOBTMP
-      FROM DUAL;
-      WLENLOB      := DBMS_LOB.getLength(WCLOBTMP);
-      WNUMLINE     :=1;
-      WHILE WOFFSET<=WLENLOB
-      LOOP
-        WROWLEN  :=instr(WCLOBTMP,chr(10),WOFFSET)-WOFFSET;
-        IF WROWLEN<0 THEN
-          WROWLEN:=WLENLOB+1-WOFFSET;
+            dbms_metadata.set_transform_param(dbms_metadata.session_transform,'EMIT_SCHEMA',false);		-- CURRENT SCHEMA
+            dbms_metadata.set_transform_param(dbms_metadata.session_transform,'SEGMENT_CREATION',false);-- NO PHYSICAL INFO
+            dbms_metadata.set_transform_param(dbms_metadata.session_transform,'PRETTY',true);			-- BEAUTYFIER
+            dbms_metadata.set_transform_param(dbms_metadata.session_transform,'STORAGE',false);			-- NO PHYSICAL INFO
+            SELECT
+                dbms_metadata.get_ddl('MATERIALIZED_VIEW',wrec.mview_name,wrec.owner) myrow
+            INTO
+                wclobtmp
+            FROM
+                dual;
+
+            wlenlob := dbms_lob.getlength(wclobtmp);
+            wnumline := 1;
+            WHILE woffset <= wlenlob LOOP
+                wrowlen := instr(wclobtmp,chr(10),woffset) - woffset;
+                IF
+                    wrowlen < 0
+                THEN
+                    wrowlen := wlenlob + 1 - woffset;
+                END IF;
+                wonerow := substr(wclobtmp,woffset,wrowlen);
+                IF
+                    regexp_like(wonerow,wsearch,'i')
+                THEN
+                    dbms_output.put_line(wrec.mview_name
+                    || wcolsep
+                    || wrec.owner
+                    || wcolsep
+                    || 'MATERIALIZED VIEW'
+                    || wcolsep
+                    || TO_CHAR(wnumline)
+                    || wcolsep
+                    || ltrim(rtrim(wonerow) ) );
+                END IF;
+
+                woffset := woffset + wrowlen + 1;
+                wnumline := wnumline + 1;
+            END LOOP;
+
         END IF;
-        WONEROW:=SUBSTR(WCLOBTMP,WOFFSET,WROWLEN);
-        IF REGEXP_LIKE(WONEROW,WSEARCH,'i') THEN
-          dbms_output.put_line( WREC.MVIEW_NAME||WCOLSEP||WREC.OWNER||WCOLSEP||'MATERIALIZED VIEW'||WCOLSEP|| TO_CHAR(WNUMLINE)||WCOLSEP||LTRIM(RTRIM(WONEROW)));
-        END IF;
-        WOFFSET  :=WOFFSET   +WROWLEN+1;
-        WNUMLINE := WNUMLINE + 1;
-      END LOOP;
-      
-      END IF;
-      WOFFSET := 1;
-      END LOOP;
+
+        woffset := 1;
+    END LOOP;
       --**********************
-  FOR WREC IN
-  (SELECT NAME,OWNER,TYPE,TEXT,LINE
-  FROM DBA_SOURCE
-  WHERE 1=1
-  AND REGEXP_LIKE(OWNER,WOBJOWNE,'i')
-  AND REGEXP_LIKE (TEXT,WSEARCH,'i')
-  AND NOT REGEXP_LIKE (TEXT,'demodocument','i')
-  )
-  LOOP
-         dbms_output.put_line( WREC.NAME||WCOLSEP||WREC.OWNER||WCOLSEP||WREC.TYPE||WCOLSEP|| TO_CHAR(WREC.LINE)||WCOLSEP||LTRIM(RTRIM(
-         REPLACE(WREC.TEXT,CHR(10),'')
-         )));
-  END LOOP;      
+
+    FOR wrec IN (
+        SELECT
+            name,
+            owner,
+            type,
+            text,
+            line
+        FROM
+            dba_source
+        WHERE
+            1 = 1
+            AND   REGEXP_LIKE ( owner,
+            wobjowne,
+            'i' )
+            AND   REGEXP_LIKE ( text,
+            wsearch,
+            'i' )
+-- OTHER CONDITIONS TO AVOID "NOISE"  					
+--   AND NOT REGEXP_LIKE (TEXT,'demodocument','i')						-- CHECK HERE!
+    ) LOOP
+        dbms_output.put_line(wrec.name
+        || wcolsep
+        || wrec.owner
+        || wcolsep
+        || wrec.type
+        || wcolsep
+        || TO_CHAR(wrec.line)
+        || wcolsep
+        || ltrim(rtrim(replace(wrec.text,chr(10),'') ) ) );
+    END LOOP;      
   --**********************      
+
 END;
 /
