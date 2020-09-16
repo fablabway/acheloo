1dc2147bdb5a450b4a444f066003ef183e3bcd1d
fablabway <68069401+fablabway@users.noreply.github.com>
2020-09-16 14:05:02 +0200

    Add files via upload
    FIXED FOR MSG

--- youdir.sql
+++ youdir.sql
@@ -15,46 +15,46 @@ SET SERVEROUTPUT ON SIZE 1000000
 
 DECLARE
     V_FILE      UTL_FILE.FILE_TYPE;
-    VTIME number; 
-    VTIME2 number; 
-    WMIADIR varchar2(40);
-    WTHISONE varchar2(30) := 'YOUDIR';
-    wmiopath varchar2(80);
-    wmsgsentence varchar2(100);
-    wmsgrow varchar2(4000);
-    wmsgrowbytes number;
-    whowmany number;
-    wnumloop number;
+    VTIME NUMBER; 
+    VTIME2 NUMBER; 
+    WMIADIR VARCHAR2(40);
+    WTHISONE VARCHAR2(30) := 'YOUDIR (2019)';
+    WMIOPATH VARCHAR2(80);
+    WMSGSENTENCE VARCHAR2(100) := 'ROCK ''LL NEVER DIE!';
+    WMSGROW VARCHAR2(4000);
+    WMSGROWBYTES NUMBER;
+    WHOWMANY NUMBER;
+    WNUMLOOP NUMBER;
 BEGIN
-  whowmany := 50;
-  wnumloop := 4000;
-  wmsgrow := rpad(wmsgsentence,whowmany*LENGTH(wmsgsentence),wmsgsentence);
-  select vsize(wmsgrow)*wnumloop into wmsgrowbytes from dual;
-  dbms_output.put_line(WTHISONE ||' - https://github.com/fablabway/acheloo');
-  dbms_output.put_line('==> Oracle Dire performances');
-  dbms_output.put_line('==> write a file '||to_char(wmsgrowbytes)||' bytes long');
-  dbms_output.put_line(rpad('-',50,'-'));
-  FOR WDIR IN (SELECT DIRECTORY_NAME,DIRECTORY_PATH FROM ALL_DIRECTORIES 
+  WHOWMANY := 10;
+  WNUMLOOP := 4000;
+  WMSGROW := RPAD(WMSGSENTENCE||' ',WHOWMANY*LENGTH(WMSGSENTENCE),WMSGSENTENCE);
+  SELECT NVL(VSIZE(WMSGROW)*WNUMLOOP,0) INTO WMSGROWBYTES FROM DUAL;
+  DBMS_OUTPUT.PUT_LINE(WTHISONE ||' - https://github.com/fablabway/acheloo');
+  DBMS_OUTPUT.PUT_LINE('==> ORACLE DIRE PERFORMANCES');
+  DBMS_OUTPUT.PUT_LINE('==> WRITE A FILE '||TO_CHAR(WMSGROWBYTES)||' BYTES LONG');
+  DBMS_OUTPUT.PUT_LINE(RPAD('-',50,'-'));
+  FOR WDIR IN (SELECT DIRECTORY_NAME,DIRECTORY_PATH FROM ALL_DIRECTORIES ORDER BY DIRECTORY_NAME
 )
   LOOP
-  begin
+  BEGIN
     VTIME := DBMS_UTILITY.GET_TIME();
     WMIADIR := WDIR.DIRECTORY_NAME;
-    wmiopath := WDIR.DIRECTORY_PATH;
-    V_FILE := UTL_FILE.FOPEN(wmiadir, 'testmr.txt', 'W');
-    for WNDX in 0..wnumloop LOOP
-      UTL_FILE.PUT_LINE (v_file, 'Line '||to_char(wndx)||' '||wmsgrow);
-    end loop;
+    WMIOPATH := WDIR.DIRECTORY_PATH;
+    V_FILE := UTL_FILE.FOPEN(WMIADIR, 'TESTMR.TXT', 'W');
+    FOR WNDX IN 0..WNUMLOOP-1 LOOP
+      UTL_FILE.PUT_LINE (V_FILE, 'LINE '||TO_CHAR(WNDX)||' '||WMSGROW);
+    END LOOP;
     UTL_FILE.FFLUSH (V_FILE);
     UTL_FILE.FCLOSE (V_FILE);
     VTIME2 := DBMS_UTILITY.GET_TIME();
-    DBMS_OUTPUT.PUT_LINE('writing on '||WMIADIR||' ('||wmiopath||') in '||TO_CHAR(((VTIME2-VTIME)*10))||'(ms)');
+    DBMS_OUTPUT.PUT_LINE('WRITING ON '||WMIADIR||' ('||WMIOPATH||') IN '||TO_CHAR(((VTIME2-VTIME)*10))||'(MS)');
     EXCEPTION
-   when others then
-   dbms_output.put_line('*** ERROR ON LOOP '||rtrim(wmiadir)||'('||wmiopath||') - ORA'||SQLCODE);  
-    end;
-  end loop;
-  dbms_output.put_line(rpad('-',50,'-'));
-end;
+   WHEN OTHERS THEN
+   DBMS_OUTPUT.PUT_LINE('*** ERROR ON LOOP '||RTRIM(WMIADIR)||'('||WMIOPATH||') - ORA'||SQLCODE);  
+    END;
+  END LOOP;
+  DBMS_OUTPUT.PUT_LINE(RPAD('-',50,'-'));
+END;
 /
 
